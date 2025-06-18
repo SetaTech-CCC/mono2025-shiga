3e44a0d01a680f54eb7931417d04ec6e931fcfa8
Syuugo <pub@s1204.me>
2025-06-18 20:16:49 +0900

    ディレクトリ自動作成スクリプトを追加

---
+++ kadai_mkdir.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal EnableDelayedExpansion
+title kadai フォルダの作成
+
+cls
+cd "%~dp0"
+echo;
+
+set mkdirs=
+:check
+set /p mkdirs="作成するフォルダ数を入力："
+if "%mkdirs%"=="" goto :check
+
+cls
+echo;
+for /l %%i in (1,1,!mkdirs!) do (
+  mkdir kadai%%i
+  echo f | xcopy Inspector\mono2025.h kadai%%i\mono2025.h /y /h /q > nul
+  :attrib -h kadai%%i\mono2025.h>nul
+  echo f | xcopy temp.c kadai%%i\kadai%%i.ino /y /h /q > nul
+  attrib -h kadai%%i\kadai%%i.ino>nul
+  if %ErrorLevel% equ 0 echo kadai%%i を作成しました。
+  echo;
+)
+attrib +h temp.c > nul
+attrib +h kadai_mkdir.bat > nul
+
+start explorer "%~dp0"
+echo;
+timeout /t 3 /nobreak
