f30d1bddd09445d90fdd593dbd6ff3407ab1f8dc
Syuugo <pub@s1204.me>
2025-06-23 23:26:06 +0900

    kadai5 を最適化

--- kadai5/kadai5.ino
+++ kadai5/kadai5.ino
@@ -3,23 +3,14 @@
 void loop() {
   boolean tact_l = isTactEnabled(LS);
   boolean tact_r = isTactEnabled(RS);
-  if (isToggleEnabled()) {
-    if (tact_l) {
-      dc(RT);
-    } else if (tact_r) {
-      dc(LT);
-    } else if (!tact_l && !tact_r) {
-      dc(F);
-    }
-  } else {
-    if (tact_l) {
-      dc(RT);
-      dc(S);
-    } else if (tact_r) {
-      dc(LT);
-      dc(S);
-    } else if (!tact_l && !tact_r) {
-      dc(F);
-    }
-  } 
+  boolean toggle = isToggleEnabled();
+  if (tact_l) {
+    dc(RT);
+    if (toggle) dc(S);
+  } else if (tact_r) {
+    dc(LT);
+    if (toggle) dc(S);
+  } else if (!tact_l && !tact_r) {
+    dc(F);
+  }
 }
